e3c256a75fef0b93fc66060124f9325a7c096fc2
clc <devnull@localhost>
2005-03-28 17:22:31 -0500

    first version of import accounts

---
+++ import/import_accounts.pl
@@ -0,0 +1,122 @@
+#!/usr/local/bin/perl -w
+
+use strict;
+use DBI;
+
+my $passwd_file = "/maytag/sys0/NIS/src/passwd.NEW";
+my $group_file = "/maytag/sys0/NIS/src/group";
+my $usage_file = "/maytag/sys0/NIS/src/usage.byID";
+my $identity_file = "/maytag/sys0/NIS/src/identity";
+
+my $dbh = DBI->connect("dbi:Pg:dbname=udb;host=db", 'clc', 'pr0bl3m')
+	or die "Couldn't connect to database: " . DBI->errstr;
+
+open(INPUT, "<$group_file") or die("Couldn't open file: $group_file");
+
+while(<INPUT>) {
+
+	chomp($_);
+	if ($_ eq "") {next;}
+
+	my @list = split /:/,	$_;
+
+	print "adding group: $list[0] \tgid: $list[2]\n";	
+
+	my $sth_create = $dbh->prepare("INSERT INTO groups (group_name, gid) VALUES (?, ?)");
+	$sth_create->execute($list[0], $list[2]);
+	$sth_create->finish;
+}
+
+close(INPUT);
+open(INPUT, "<$passwd_file") or die("Couldn't open file: $passwd_file");
+
+while(<INPUT>) {
+
+	chomp($_);
+	if ($_ eq "") {next;}
+
+	my @list = split /:/,	$_;
+	my @info = split /,/,	$list[4];
+
+	print "adding account: $list[0]\n";
+
+	my $sth = $dbh->prepare("SELECT nextval FROM nextval('person_id_seq')");
+	$sth->execute;
+	my $row = $sth->fetchrow_hashref;
+	my $person_id = $row->{nextval};
+	$sth->finish;
+
+	$sth = $dbh->prepare("INSERT INTO people (person_id, full_name, office, phone) VALUES (?,?,?,?)");
+	$sth->execute($person_id, $info[0], $info[1], $info[2]);
+	$sth->finish;
+
+	$sth = $dbh->prepare("INSERT INTO accounts (uid, person_id, login, gid, shell, home_dir, dirty) VALUES (?, ?, ?, ?, ?, ?, 'changed')");
+	$sth->execute($list[2], $person_id, $list[0], $list[3], $list[6], $list[5]);
+	$sth->finish;
+}
+close(INPUT);
+open(INPUT, "<$group_file") or die("Couldn't open file: $group_file");
+
+while(<INPUT>) {
+
+        chomp($_);
+	if ($_ eq "") {next;}
+
+        my @list = split /:/,   $_;
+	if (!defined($list[3])) {next;}
+	my @grplst = split /,/,   $list[3];
+
+	foreach (@grplst) {
+			
+		print "adding login $_ to group $list[0]\n";
+		my $sth = $dbh->prepare("INSERT INTO group_list (group_name, login) VALUES (?, ?)");
+        	$sth->execute($list[0], trimwhitespace($_));
+        	$sth->finish;
+	}
+}
+close(INPUT);
+open(INPUT, "<$usage_file") or die("Couldn't open file: $usage_file");
+
+while(<INPUT>) {
+
+        chomp($_);
+        if ($_ eq "") {next;}
+        my @list = split /:/,   $_;
+        if (!defined($list[1])) {next;}
+
+        print "adding identity $list[0]:$list[1]";
+        my $sth = $dbh->prepare("INSERT INTO identities (identity, space) VALUES ($,$)");
+        $sth->execute($list[0], $list[1]);
+        $sth->finish;
+}
+close(INPUT);
+open(INPUT, "<$identity_file") or die("Couldn't open file: $identity_file");
+
+while(<INPUT>) {
+
+        chomp($_);
+        if ($_ eq "") {next;}
+
+        my @list = split /:/,   $_;
+        if (!defined($list[1])) {next;}
+        my @identities = split /,/,   $list[1];
+
+        foreach (@identities) {
+
+                print "adding identity $_ to login $list[0]\n";
+                my $sth = $dbh->prepare("INSERT INTO identity_list (identity, login) VA$
+                $sth->execute(trimwhitespace($_),trimwhitespace($list[0]));
+                $sth->finish;
+        }
+}
+close(INPUT);
+$dbh->disconnect || die "Can't disconnect from database: $DBI::errstr\n";
+
+
+sub trimwhitespace($)
+{
+	my $string = shift;
+	$string =~ s/^\s+//;
+	$string =~ s/\s+$//;
+	return $string;
+}
