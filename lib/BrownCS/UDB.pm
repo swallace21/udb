9db4e2ad50f67caa1c432cf705c85626945ed98a
Aleks Bromfield <aleks@cs.brown.edu>
2009-02-02 18:14:18 -0500

    frontend work

--- lib/BrownCS/UDB.pm
+++ lib/BrownCS/UDB.pm
@@ -7,11 +7,14 @@ use warnings;
 use DBI qw(:sql_types);
 use DBD::Pg qw(:pg_types);
 use Term::ReadKey;
+use Term::ReadLine;
 use Pod::Usage;
-use Data::Dumper;
 
 our $PNAME = 'UDB';
 our $VERSION = '0.01';
+
+my $term = new Term::ReadLine 'udb';
+$term->ornaments(0);
 
 #
 # static methods
@@ -33,12 +36,35 @@ sub get_pass {
 # ynquery :: string -> boolean
 # Prompt the user for yes or no and return appropriate value.
 sub ynquery {
-  my($prompt) = @_;
-  my($answer);
+  my($prompt, $default) = @_;
+  while (1) {
+    my $answer = $term->readline($prompt);
+    if ($answer eq '') {
+      return $default;
+    } elsif ($answer =~ /^y(es)?$/i) {
+      return 1;
+    } elsif ($answer =~ /^n(o)?$/i) {
+      return 0;
+    } else {
+      print "Invalid answer. Please answer yes or no (y/n).\n"
+    }
+  }
+}
 
-  printflush('STDOUT', $prompt);
-  chop($answer = <STDIN>);
-  return ($answer eq '') || ($answer eq 'Y') || ($answer eq 'y');
+# textquery :: string -> string
+# Gets a line from the user.
+sub textquery {
+  my($prompt, $default) = @_;
+  while (1) {
+    my $answer = $term->readline($prompt);
+    if (($answer eq '') and $default) {
+      return $default;
+    } elsif ($answer ne '') {
+      return $answer;
+    } else {
+      print "Invalid answer. Please enter a non-empty string.\n"
+    }
+  }
 }
 
 #
@@ -121,7 +147,7 @@ sub get_host {
 
   $self->prepare("aliases_select", "select nde.name from net_dns_entries nde, equipment e, net_addresses_net_interfaces nani, net_interfaces ni, net_addresses na where e.name = ? and e.id = ni.equipment_id and nani.net_interfaces_id = ni.id and nani.net_addresses_id = na.id and na.id = nde.net_address_id");
   $self->prepare("classes_select", "select cc.class from comp_classes cc, computers c, comp_classes_computers ccc, equipment e where e.name = ? and e.id = c.equipment_id and ccc.comp_classes_id = cc.id and ccc.computers_id = c.id");
-  $self->prepare("comp_select", "select c.id, c.hw_arch, c.os, c.pxelink, from equipment e, computers c where e.name = ? and c.equipment_id = e.id");
+  $self->prepare("comp_select", "select c.id, c.hw_arch, c.os, c.pxelink from equipment e, computers c where e.name = ? and c.equipment_id = e.id");
   $self->prepare("equip_select", "select e.id, e.contact, e.equip_status from equipment e where e.name = ?");
   $self->prepare("ethernet_select", "select ni.ethernet from equipment e, net_interfaces ni where e.name = ? and e.id = ni.equipment_id");
   $self->prepare("ip_addr_select", "select na.ipaddr from equipment e, net_addresses_net_interfaces nani, net_interfaces ni, net_addresses na where e.name = ? and e.id = ni.equipment_id and nani.net_interfaces_id = ni.id and nani.net_addresses_id = na.id");
@@ -218,6 +244,120 @@ sub get_vlan {
   }
 
   return $vlan_id;
+}
+
+sub insert_host {
+  my $self = shift;
+  my($host) = @_;
+
+  $self->prepare("equip_insert", "INSERT INTO equipment (equip_status, usage, name, contact) VALUES ('deployed', 'academic', ?, ?) RETURNING id");
+
+  $self->prepare("comp_insert", "INSERT INTO computers (equipment_id, hw_arch, os, pxelink) VALUES (?, ?, ?, ?) RETURNING id");
+  $self->{sths}->{comp_insert}->bind_param(1, undef, SQL_INTEGER);
+
+  $self->prepare("interface_insert", "INSERT INTO net_interfaces (equipment_id, ethernet) VALUES (?, ?) RETURNING id");
+  $self->{sths}->{interface_insert}->bind_param(1, undef, SQL_INTEGER);
+  $self->{sths}->{interface_insert}->bind_param(2, undef, {pg_type => PG_MACADDR});
+
+  $self->prepare("address_insert", "INSERT INTO net_addresses (vlan_id, ipaddr, monitored) VALUES (?, ?, ?) RETURNING id");
+  $self->{sths}->{address_insert}->bind_param(1, undef, SQL_INTEGER);
+  $self->{sths}->{address_insert}->bind_param(2, undef, {pg_type => PG_INET});
+  $self->{sths}->{address_insert}->bind_param(3, undef, {pg_type => PG_BOOL});
+
+  $self->prepare("addr_iface_insert", "INSERT INTO net_addresses_net_interfaces (net_addresses_id, net_interfaces_id) VALUES (?, ?)");
+  $self->{sths}->{addr_iface_insert}->bind_param(1, undef, SQL_INTEGER);
+  $self->{sths}->{addr_iface_insert}->bind_param(2, undef, SQL_INTEGER);
+
+  $self->prepare("dns_insert", "INSERT INTO net_dns_entries (name, domain, zone, net_address_id) VALUES (?, 'cs.brown.edu', 'both', ?)");
+  $self->{sths}->{dns_insert}->bind_param(2, undef, SQL_INTEGER);
+
+  $self->prepare("class_comp_insert", "INSERT INTO comp_classes_computers (comp_classes_id, computers_id) VALUES (?,?)");
+  $self->{sths}->{class_comp_insert}->bind_param(1, undef, SQL_INTEGER);
+  $self->{sths}->{class_comp_insert}->bind_param(2, undef, SQL_INTEGER);
+
+  my $machine_name = $host->{'hostname'};
+
+  print "importing cdb $machine_name";
+
+  my $contact = $host->{'contact'};
+
+  $host->{'aliases'} =~ s/\s//g;
+  my @aliases = split(/,/, $host->{'aliases'});
+
+  $host->{'classes'} =~ s/\s//g;
+  my @classes = split(/,/, $host->{'classes'});
+
+  my $ethernet = $host->{'ethernet'};
+
+  my $ipaddr = $host->{'ip_addr'};
+  my $vlan_id;
+  if ((!$ipaddr) or ($ipaddr eq "")) {
+    $ipaddr = undef;
+    $vlan_id = $self->get_vlan("128.148.36.1");
+  } else {
+    $vlan_id = $self->get_vlan($ipaddr);
+  }
+
+  my $status = $host->{'status'};
+
+  my $monitored = 0;
+  if ($status eq "monitored") {
+    $monitored = 1;
+  }
+
+  # create an equipment entry...
+
+  $self->{sths}->{equip_insert}->execute($machine_name, $contact);
+  my $equip_id = $self->{sths}->{equip_insert}->fetch()->[0];
+
+  # fill in equip_status
+
+  my $hw_arch = $host->{'hw_arch'};
+  if ($hw_arch eq "") {
+    $hw_arch = undef;
+  } elsif ($hw_arch eq "x64") {
+    $hw_arch = "amd64";
+  }
+
+  my $os = $host->{'os_type'};
+  if (($os eq "other") or ($os eq "") or ($os eq "windows")) {
+    $os = undef;
+  }
+
+  my $pxelink = $host->{'pxelink'};
+  if ($pxelink eq "") {
+    $pxelink = undef;
+  }
+
+  $self->{sths}->{comp_insert}->execute($equip_id, $hw_arch, $os, $pxelink);
+  my $comp_id = $self->{sths}->{comp_insert}->fetch()->[0];
+
+  $self->{sths}->{interface_insert}->execute($equip_id, $ethernet);
+  my $interface_id = $self->{sths}->{interface_insert}->fetch()->[0];
+
+  $self->{sths}->{address_insert}->execute($vlan_id, $ipaddr, $monitored);
+  my $address_id = $self->{sths}->{address_insert}->fetch()->[0];
+
+  $self->{sths}->{addr_iface_insert}->execute($address_id, $interface_id);
+
+  $self->{sths}->{dns_insert}->execute($machine_name, $address_id);
+
+  if ( $#aliases != -1 ) {
+    foreach (@aliases) {
+      print ", alias $_";
+      $self->{sths}->{dns_insert}->execute($_, $address_id);
+    }
+  }
+
+  if ( $#classes != -1 ) {
+    foreach (@classes) {
+      print ", class $_";
+      my $class_id = $self->get_class($_);
+      $self->{sths}->{class_comp_insert}->execute($class_id, $comp_id);
+    }
+  }
+
+  print "\n";
 }
 
 # sub find_unused_ip {
