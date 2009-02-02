b6a119703025fcddb925a82fe62dde74f112e10c
Aleks Bromfield <aleks@cs.brown.edu>
2009-02-02 18:24:28 -0500

    moving stuff around

--- src/build_scripts/fixloc.sql
+++
@@ -1,8 +0,0 @@
-begin;
-update location set building = 'CIT', room = location.lid where lid ~* '^[0-9][0-9][0-9]$';
-update location set floor = '1' where room ~* '^1[0-9][0-9]$';
-update location set floor = '2' where room ~* '^2[0-9][0-9]$';
-update location set floor = '3' where room ~* '^3[0-9][0-9]$';
-update location set floor = '4' where room ~* '^4[0-9][0-9]$';
-update location set floor = '5' where room ~* '^5[0-9][0-9]$';
-commit;
