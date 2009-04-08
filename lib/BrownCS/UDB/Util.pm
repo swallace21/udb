8452851d2a11ea26e891b418e6a5d0e6c535128b
Aleks Bromfield <aleks@cs.brown.edu>
2009-04-08 19:11:51 -0400

    more more typos

--- lib/BrownCS/UDB/Util.pm
+++ lib/BrownCS/UDB/Util.pm
@@ -21,7 +21,6 @@ our @EXPORT_OK = qw(
   fmt_time
   get_date
   ipv4_n2x
-  fix_date
   get_new
   bool
 );
@@ -176,60 +175,6 @@ sub fix_width {
   my $spaces = ' ' x ($width - length($str));
   return ($str . $spaces);
 };
-
-sub fix_date {
-  my($orig) = shift;
-  my($month, $day, $year);
-
-  if ( $orig =~ m%^\d+[/-]\d+$% ) {
-    ($month, $year) = ($1, $2);
-    $year = &fix_year($year);
-    return "$year-$month-01";
-  }
-
-  if ( $orig =~ m%\d+[/-]\d+[/-]\d+$% ) {
-    ($month, $day, $year) = ($1, $2, $3);
-    $year = &fix_year($year);
-    return "$year-$month-$day";
-  }
-
-  if ( $orig =~ m%^\d+[/-][A-Za-z]+[/-]\d+$% ) {
-    ($day, $month, $year) = ($1, $2, $3);
-    $year = &fix_year($year);
-    $month = &fix_month($month);
-    return "$year-$month-$day";
-  }
-  return undef;
-}
-
-sub fix_month {
-  my($month) = shift;
-
-  if ( $month =~ /Jun/ ) {
-    6;
-  }
-  elsif ( $month =~ /May/) {
-    5;
-  }
-  else {
-    '';
-  }
-}
-
-sub fix_year {
-  my($year) = shift;
-
-  if ( $year > 100 ) {
-    return $year;
-  }
-
-  if ( $year =~ /^0/ ) {
-    $year = '20' . $year;
-  }
-  else {
-    $year = '19' . $year;
-  }
-}
 
 sub get_new {
   my ($maybe, $desc, $verify_proc) = @_;
