a05c9c6e82f683d0834847d848fa2c5826ea0d42
Aleks Bromfield <aleks@cs.brown.edu>
2009-05-04 16:55:40 -0400

    netgroup fixes

--- lib/BrownCS/udb/Schema/OsTypes.pm
+++ lib/BrownCS/udb/Schema/OsTypes.pm
@@ -8,20 +8,9 @@ use base 'DBIx::Class';
 __PACKAGE__->load_components("Core");
 __PACKAGE__->table("os_types");
 __PACKAGE__->add_columns(
-  "os_type",
-  {
-    data_type => "text",
-    default_value => undef,
-    is_nullable => 0,
-    size => undef,
-  },
-  "pxe_boot",
-  {
-    data_type => "boolean",
-    default_value => "false",
-    is_nullable => 0,
-    size => 1,
-  },
+  "os_type"     => { data_type => "text", default_value => undef, is_nullable => 0, size => undef, },
+  "pxe_boot"    => { data_type => "boolean", default_value => "false", is_nullable => 0, size => 1, },
+  "trusted_nfs" => { data_type => "boolean", default_value => "false", is_nullable => 0, size => 1, },
 );
 __PACKAGE__->set_primary_key("os_type");
 __PACKAGE__->add_unique_constraint("os_types_pkey", ["os_type"]);
@@ -42,10 +31,4 @@ __PACKAGE__->has_many(
   }
 );
 
-
-# Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-04-28 16:23:19
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:IPvamFd3sin0Hmq931xFoA
-
-
-# You can replace this text with custom content, and it will be preserved on regeneration
 1;
